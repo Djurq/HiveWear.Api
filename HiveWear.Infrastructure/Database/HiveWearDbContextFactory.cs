9ff3af11b54fe841a48d713e7fea0847870776ad
Djurq <djurrebax@gmail.com>
2025-05-25 19:35:22 +0200

    werkt deze factory?

---
+++ HiveWear.Infrastructure/Database/HiveWearDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HiveWear.Infrastructure.Database
+{
+    public class HiveWearDbContextFactory : IDesignTimeDbContextFactory<HiveWearDbContext>
+    {
+        public HiveWearDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Directory.GetCurrentDirectory();
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.Production.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<HiveWearDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new HiveWearDbContext(optionsBuilder.Options);
+        }
+    }
+}
