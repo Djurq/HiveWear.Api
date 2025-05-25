d0adb2affa770f8b35ca0ad73849a8e06d6fef1b
Djurq <djurrebax@gmail.com>
2025-05-25 19:42:15 +0200

    fix database hopefully

--- HiveWear.Infrastructure/Migrations/20250525174150_fix.Designer.cs
+++ HiveWear.Infrastructure/Migrations/20250525174150_fix.Designer.cs
@@ -4,6 +4,7 @@ using HiveWear.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HiveWear.Infrastructure.Migrations
 {
     [DbContext(typeof(HiveWearDbContext))]
-    partial class HiveWearDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250525174150_fix")]
+    partial class fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
